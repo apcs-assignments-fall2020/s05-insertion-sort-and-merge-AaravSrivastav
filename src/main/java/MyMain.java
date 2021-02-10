aff7cd7c8a11fc837dd6510f87d23c587ef10d23
AaravSrivastav <22asrivastav@athenian.org>
2021-02-10 02:59:59 +0000

    did the HW

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -1,19 +1,37 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class MyMain {
 
     // Sorts the ArrayList using insertion sort
-    public static ArrayList<Integer> insertionSort(ArrayList<Integer> list) {
-        // YOUR CODE HERE
-        return null;
+    public static ArrayList<Integer> insertionSort(ArrayList<Integer> list)
+    {
+        for (int i = 1; i < list.size(); i++) 
+        {
+            int a = list.get(i);
+            int j = i - 1;
+            while ((j > -1) && (list.get(j).compareTo(a) == 1)) 
+            {
+                list.set(j + 1, list.get(j));
+                j--;
+            }
+            list.set(j + 1, a);
+        }
+        return list;
     }
 
      // Merges two sorted arrays into one large combined
      // sorted array
      // You may assume arr1 and arr2 are the same length
     public static int[] merge(int[] arr1, int[] arr2) { 
-        // YOUR CODE HERE
-        return null;
+        int[] newArr = new int[arr1.length*2];
+        for(int i = 0; i<arr1.length; i++)
+        {
+            newArr[2*i]=arr1[i];
+            newArr[2*i+1]=arr2[i];
+        }
+        Arrays.sort(newArr);
+        return newArr;
     }
 
     public static void main(String[] args) {
